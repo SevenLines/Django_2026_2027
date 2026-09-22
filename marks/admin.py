85d51a24c43e69626092f8475d60c4a64aa82958
katash <katash@ex.istu.edu>
2026-09-22 21:47:48 +0800

    add profile and media

--- marks/admin.py
+++ marks/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from marks.models import Discipline, Profile
+
+
 # Register your models here.
+@admin.register(Discipline)
+class DisciplineAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False
--- marks/models.py
+++ marks/models.py
@@ -1,4 +1,27 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    class Role(models.IntegerChoices):
+        undefined = 0
+        teacher = 1
+        student = 2
+
+    user = models.OneToOneField("auth.User", on_delete=models.CASCADE, null=True, blank=True)
+    role = models.IntegerField("Роль", choices=Role, default=Role.undefined)
+    group = models.ForeignKey("marks.Group", on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username if self.user else f"Profile {self.id}"
+
+
+@receiver(post_save, sender=User)
+def on_user_create(sender, instance, created, *args, **kwargs):
+    if created:
+        Profile.objects.create(user=instance, role=Profile.Role.undefined)
+
 
 # Create your models here.
 class Group(models.Model):
@@ -9,8 +32,9 @@ class Group(models.Model):
 
 class Discipline(models.Model):
     title = models.TextField()
-    group = models.ForeignKey("marks.Group", on_delete=models.CASCADE, null=True)
-    teacher = models.ForeignKey("auth.User", on_delete=models.CASCADE, null=True)
+    group = models.ForeignKey("marks.Group", on_delete=models.CASCADE, null=True, blank=True)
+    teacher = models.ForeignKey("auth.User", on_delete=models.CASCADE, null=True, blank=True)
+    image = models.ImageField("Картинка", null=True, upload_to='discipline')
 
 
 class Lesson(models.Model):
